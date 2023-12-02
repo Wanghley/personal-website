ca98280fecd8b7033353120b9a424b8015db0e59
Wanghley Soares Martins <wanghleys@gmail.com>
2023-12-02 00:31:40 -0500

    feat: initial react app work

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Wanghley – Sci & Tech website coming soon.
+        </p>
+        <a
+          className="App-link"
+          href="https://instagram.com/wanghley"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See more
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

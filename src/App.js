678c3d934f55c8a6017d8752a9ad70c6f2820f60
Wanghley Soares Martins <wanghleys@gmail.com>
2023-12-02 00:31:42 -0500

    fix: remove unecessary imports and add skills component in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { Navbar, Hero, About, Contact } from './components'
+import { Navbar, Hero, About, Contact, Skills } from './components'
 
 function App() {
   return (
@@ -8,6 +7,7 @@ function App() {
     <Navbar />
     <Hero />
     <About />
+    <Skills />
     <Contact />
     </>
   );

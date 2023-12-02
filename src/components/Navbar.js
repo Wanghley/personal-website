d163adf1012e869e4972bea7d62be466bbae1b9d
Wanghley Soares Martins <wanghleys@gmail.com>
2023-12-02 00:31:40 -0500

    feat: navbar initial work

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../assets/logo-white.svg'
+import './css/Navbar.css'
+// import { navLinks } from '../constants'
+
+const Navbar = () => {
+
+  return (
+    <nav>
+      <img src={logo} alt='wanghley'/>
+    </nav>
+  )
+}
+
+export default Navbar

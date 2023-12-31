668aa6191b5fa51d7512c0154cb3d92d314e9a4b
Wanghley Soares Martins <wanghleys@gmail.com>
2023-12-31 11:22:16 -0300

    feat: implement timeline api fetch with axios

---
+++ src/api/timeline.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const baseURL = process.env.REACT_APP_timeline_api_url;
+
+export async function fetchTimeline(nextPage = 1, prevData = null) {
+  try {
+    const url = `${baseURL}?pagination[page]=${nextPage}`;
+    const response = await axios.get(url);
+
+    const updatedData = {
+        ...response.data,
+        data: prevData ? [...prevData.data, ...response.data] : response.data
+    };
+    // const updatedData = {
+    //   ...response.data.data,
+    //   data: prevData ? [...prevData.data.data, ...response.data.data] : response.data.data
+    // };
+
+    if (response.data.meta.pagination.pageCount <= nextPage+1) { // If there are no more pages, return the data
+        console.log(updatedData.data.data);
+        return updatedData.data.data;
+    } else {
+      return fetchTimeline(nextPage + 1, updatedData);
+    }
+  } catch (e) {
+    console.error(e);
+    throw e; // Rethrow the error for the caller to handle
+  }
+}
+
+export default fetchTimeline;

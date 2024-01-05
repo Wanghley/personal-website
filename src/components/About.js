4d10b68368c32199d7388c20d549925f06ae097b
Wanghley Soares Martins <wanghleys@gmail.com>
2024-01-05 20:44:24 -0300

    fix: about text on homepage update

--- src/components/About.js
+++ src/components/About.js
@@ -23,11 +23,18 @@ const About = () => {
             <div className='about'>
                 <div className='about-text'>
                     <h1 className='about-title'>About Me</h1>
-                    <h2 className='about-subtitle'>A very curious, problem-driven, and inventive guy solving real-world problems and discovering the world through multi-faceted lens.</h2>
+                    <h2 className='about-subtitle'>🎼 A Maestro of Curiosity 🚀 Crafting Tomorrow's Tech Marvels 🌟 One Ingenious Solution at a Time ⚙️ Shaping the Future, and Beyond! 🚀🔮</h2>
                     <img src={about_photo} alt='Wanghley' className='about-photo-mobile lazyload'/>
-                    <p className='about-paragraph'>An engineering student at Duke University in the Pratt School of Engineering in the class of 2026, Wanghley, a native of Brasília, DF, Brazil, is passionate about the union of computer science, electrical/biomedical engineering and quantum computing for the development of health tech solutions.</p>
-                    <p className='about-paragraph'>Wanghley is a computer technician graduated from the Federal Institute of Education, Science, and Technology of Brasilia; a social entrepreneur from the Watson Institute Semester Accelerator Fall 2021 in Boulder, Colorado, USA; a data scientist and analyst; and a developer of expert systems. He developed a frequency system using computer vision and, mainly, an end-to-end system for the diagnosis and prognosis of neurodegenerative diseases. Working mainly in artificial intelligence and wearable technologies aimed at the health area.</p>
-                    <p className='about-paragraph'>Multi-award winner and present in several Brazilian and international science and engineering events/competitions. Selected also as one of the 4 young inventors by the Brazilian Ministry of Science, Technology, and Innovation in the international cooperation BRICS YSF in Bangaluru, India in 2021, besides presence in the National Youth Science Camp and Regeneron ISEF.</p>
+
+                    <p className='about-paragraph'>Wanghley is a relentless <strong>tech wizard</strong> rooted in the heart of Brasília, Brazil, and with an unconventional childhood spent on a pig farm 🐷. Now a dynamic aspirant <u>electrical and computer engineer</u> at Duke University, Wanghley is a <strong>curious problem-solver</strong> weaving his journey through computer science, electrical/biomedical engineering, and quantum computing.</p>
+
+                    <p className='about-paragraph'>A <strong>Karsh Scholar @ Duke</strong> and a <strong>Líder Estudar Fellow @ Fundação Estudar</strong>, is not defined by accolades but by a mission. His toolbox? a blend of technical, management, soft skills – a symphony of skills cultivated in the pig farm and refined in the world of <u>science, engineering, and technology</u>.</p>
+
+                    <p className='about-paragraph'>Beyond algorithms and circuits, he's a <strong>social entrepreneur</strong> with a flair for data science, a knack for innovation, and a passion for health tech. His projects include a frequency system with computer vision and a groundbreaking end-to-end solution for neurodegenerative diseases. These aren't just innovations; they're the echoes of a journey from pigsty to tech excellence.</p>
+
+                    <p className='about-paragraph'>Wanghley's narrative is a fusion of relentless ambition, a commitment to societal impact, and the magic of a pig farm childhood. Each breakthrough is not just a tech triumph; it's a promise of a future where his roots and wings coalesce into a story of <b>passion, problem-solving, and profound innovation</b>.</p>
+
+
                     <button className='secondary-button about-button'>Know me more</button>
                 </div>
                 <div className='about-image'>
